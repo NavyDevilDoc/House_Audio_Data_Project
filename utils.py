b90a16fd60532744e77671c965b041cb15a321ca
NavyDevilDoc <79477977+NavyDevilDoc@users.noreply.github.com>
2023-09-16 23:14:32 -0400

    Update utils.py
    Updates to match the modular nature of the test branch

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ import numpy as np
 import time
 
 def write_to_csv(filename, data):
-    with open(filename, "a", newline="") as avgfile:
+    with open(filename, "w", newline="") as avgfile:  # Note the "w" here
         avg_writer = csv.writer(avgfile)
         avg_writer.writerow(["Timestamp", "Counter", "Rolling_Avg (ADC)", "Rolling_Avg (dB)"])
         avg_writer.writerows(data)
@@ -12,5 +12,19 @@ def write_to_csv(filename, data):
 def calc_rolling_avg(samples):
     return np.convolve(samples, np.ones(10)/10, mode='valid')
 
+
 def get_current_timestamp():
     return time.strftime("%Y-%m-%d %H:%M:%S")
+
+
+def track_time(start_time, last_reset_time, minute_counter, SAMPLING_INTERVAL):
+    elapsed_since_reset = time.time() - last_reset_time
+    elapsed_time = time.time() - start_time
+    
+    if elapsed_since_reset >= (minute_counter + 1) * 60:
+        minute_counter += 1
+        print(f"{minute_counter} minute(s) of data recording has elapsed.")
+    
+    time_to_save = elapsed_time >= SAMPLING_INTERVAL
+    
+    return minute_counter, time_to_save
